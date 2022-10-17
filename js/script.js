3b485b96309d5a95d97e7afdcbd55343d67bd7b2
Sondoro <olugas@yahoo.com>
2022-10-18 01:42:00 +0300

    add date and month entry validation criteria/if statements

--- js/script.js
+++ js/script.js
@@ -7,7 +7,13 @@ function getUserInputAndCalculateDay(){
   var century = parseInt(year.substring(0,2));
   year = parseInt(year);
   var month = parseInt(prompt("Enter month;"));
+   if (month <=0 || month >12){
+    alert("Invalid entry. Enter a number between 1 and 12!");
+} 
   var day = parseInt(prompt("Enter day:"));
+    if (day <=0 || day >31){
+    alert("Invalid entry. Enter a number between 1 and 31!");        
+} 
   var dayOfTheWeek = Math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
    return dayOfTheWeek   
 }
