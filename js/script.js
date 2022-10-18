a659d9fcd5f928208022685eb0b5644c3c6604ed
Sondoro <olugas@yahoo.com>
2022-10-18 03:58:18 +0300

    add empty year check and alert

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const DAYS_OF_THE_WEEK = ["Friday", "Saturday", "Sunday", "Monday", "Tuesday", "
 
 function getUserInputAndCalculateDay(){
   var year = prompt("Enter year:");
+   if (year === ""){
+    alert("Enter year. Cannot be blank!");
+}
   var century = parseInt(year.substring(0,2));
   year = parseInt(year);
   var month = parseInt(prompt("Enter month;"));
@@ -11,7 +14,7 @@ function getUserInputAndCalculateDay(){
     alert("Invalid entry. Enter a number between 1 and 12!");
 } 
   var day = parseInt(prompt("Enter day:"));
-    if (day <=0 || day >31){
+   if (day <=0 || day >31){
     alert("Invalid entry. Enter a number between 1 and 31!");        
 } 
   var dayOfTheWeek = Math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
