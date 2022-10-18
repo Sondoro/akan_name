ef2a702598decfb89a1a408f013c608253d2d3df
Sondoro <olugas@yahoo.com>
2022-10-18 17:59:42 +0300

    update gender conditions for MALE/FEMALE (UPPER CASE)

--- js/script.js
+++ js/script.js
@@ -12,27 +12,25 @@ function getUserInputAndCalculateDay(){
   var month = parseInt(prompt("Enter month (MM);"));
    if (month!=month){
     alert("Enter month (MM). Cannot be blank!");
-}   else if (month <=0 || month >12){
-    alert("Invalid entry. Enter a number between 1 and 12!");
+    } else if (month <=0 || month >12){
+      alert("Invalid entry. Enter a number between 1 and 12!");
 } 
-  var day = parseInt(prompt("Enter day (DD):")); //An invalid day should be (d<=0) or (d>31)
-   if (day===1||day===2||day===3||day===4||day===5||day===6||day===7||day===8||day===9||day===10||day===11||day===12||day===13||day===14||day===15||day===16||day===17||day===18||day===19||day===20||day===21||day===22||day===23||day===24||day===25||day===26||day===27||day===28||day===29||day===30||day===31
-    ){
-    return day;
-    } else if (day!==1||day!==2||day!==3||day!==4||day!==5||day!==6||day!==7||day!==8||day!==9||day!==10||day!==11||day!==12||day!==13||day!==14||day!==15||day!==16||day!==17||day!==18||day!==19||day!==20||day!==21||day!==22||day!==23||day!==24||day!==25||day!==26||day!==27||day!==28||day!==29||day!==30||day!==31
-    ){
-    alert("Invalid! Enter a number between 1 and 31!");        
-} 
+var day = parseInt(prompt("Enter day (DD):"));
+   if (day!=day) {
+    alert("Enter Day (DD). Cannot be blank!")
+    } else if (day <=0 || day >31){
+      alert("Invalid entry. Enter a number between 1 and 31!");        
+}  
   var dayOfTheWeek = Math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
    return dayOfTheWeek   
 }
 
 function getGender(){
   var gender = prompt("Enter your gender (Male/Female)");
-     if (gender=="Male"||gender=="Female"||gender=="male"||gender=="female"){
-       return gender;
-    }  else if (gender!="Male"||gender!="Female"||gender!="male"||gender!="female") {
-      alert("Enter Male or Female. Cannot be blank!");
+    if (gender=="Male"||gender=="Female"||gender=="male"||gender=="female"||gender=="MALE"||gender=="FEMALE"){
+      return gender;
+        } else if (gender!="Male"||gender!="Female"||gender!="male"||gender!="female"||gender!="MALE"||gender!="FEMALE") {
+          alert("Enter your gender (Male or Female). Cannot be blank!");
 }
 }
 
